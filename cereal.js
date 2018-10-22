3354d30d5c3184c6767b350faca5a57e3721b83f
doodybrains <doodybrainz@gmail.com>
2018-10-21 23:20:03 -0400

    add pop up messages and remove old images

--- cereal.js
+++ cereal.js
@@ -4,8 +4,12 @@ let log;
 const ImageArrayOne = ['images/1-1.png', 'images/1-2.png', 'images/1-3.png', 'images/2-1.png', 'images/2-2.png', 'images/2-3.png', 'images/3-1.png', 'images/3-2.png', 'images/3-3.png'];
 const ImageArrayTwo = ['images/1-1.png', 'images/1-2.png', 'images/1-3.png', 'images/2-1.png', 'images/2-2.png', 'images/2-3.png', 'images/3-1.png', 'images/3-2.png', 'images/3-3.png'];
 const ImageArrayThree = ['images/1-1.png', 'images/1-2.png', 'images/1-3.png', 'images/2-1.png', 'images/2-2.png', 'images/2-3.png', 'images/3-1.png', 'images/3-2.png', 'images/3-3.png'];
+let numberOfTrys = [];
+let video;
 
 function setup() {
+	video = createVideo('images/scary.mp4');
+	video.pause();
 	createCanvas(windowWidth, windowHeight);
 	addHTML();
 	serial = new p5.SerialPort();
@@ -14,7 +18,8 @@ function setup() {
 	createSlotSection(ImageArrayOne, '1');
 	createSlotSection(ImageArrayTwo, '2');
 	createSlotSection(ImageArrayThree, '3');
-
+	const winScreen = document.getElementById('winner');
+	winScreen.style.display = 'none';
 }
 
 function createSlotSection(array, no) {
@@ -36,32 +41,78 @@ function createSlotSection(array, no) {
 }
 
 function startSlotMachine(duration) {
+	video.pause();
 	const landedSlots = [];
+	const winScreen = document.getElementById('winner');
+	winScreen.style.display = 'none';
 
-	for(var i = 1; i < 4; i++) {
-		const slotReel = document.getElementById(`slot-${i}`);
-		let randomSpinClassNo = randomize();
-		slotReel.style = '';
-		slotReel.className = `slot `;
-		slotReel.style.animation = `spin-${randomSpinClassNo} ${duration + i * 0.05}s`;
-		slotReel.className += ` spin-${randomSpinClassNo}`;
-
-		landedSlots.push(randomSpinClassNo);
-	}
+	if (numberOfTrys.length < 6) {
+		for(var i = 1; i < 4; i++) {
+			const slotReel = document.getElementById(`slot-${i}`);
+			let randomSpinClassNo = randomize(9);
+			slotReel.style = '';
+			slotReel.className = `slot `;
+			slotReel.style.animation = `spin-${randomSpinClassNo} ${duration}s`;
+			slotReel.className += ` spin-${randomSpinClassNo}`;
 	
-	const allThree = landedSlots[0] === landedSlots[1] === landedSlots[2];
+			landedSlots.push(randomSpinClassNo);
+		}
 
-	if ( allThree ) {
-		showWinScreen();
+		setTimeout(function() { 
+			if (landedSlots[0] + landedSlots[1] + landedSlots[2] === 12 || landedSlots[0] + landedSlots[1] + landedSlots[2] === 9) {
+				showScaryScreen();
+			} else if (landedSlots[0] === landedSlots[1] === landedSlots[2]) {
+				showCandyScreen();
+			} else {
+				showTryAgainScreen();
+			}
+		}, 2300);
+	} else {
+		let randomSpinClassNo = randomize(9);
+
+		for(var i = 1; i < 4; i++) {
+			const slotReel = document.getElementById(`slot-${i}`);
+			slotReel.style = '';
+			slotReel.className = `slot `;
+			slotReel.style.animation = `spin-${randomSpinClassNo} ${duration}s`;
+			slotReel.className += ` spin-${randomSpinClassNo}`;
+			setTimeout(function() { showCandyScreen(); }, 2300);
+		}
 	}
 }
 
-function showWinScreen() {
-	console.log('you wonnn!');
+function showScaryScreen() {
+	const winScreen = document.getElementById('winner');
+	winScreen.innerHTML = '';
+	winScreen.style.display = 'flex';
+	video.parent('winner');
+	video.loop();
+	
+	setTimeout(function() {
+		video.pause();
+		video.remove();
+		winScreen.innerHTML = '<p>Add another candy!</p>';
+	}, 1800);
+
 }
 
-function randomize() {
-	return Math.floor(Math.random() * 9);
+function showCandyScreen() {
+	const winScreen = document.getElementById('winner');
+	winScreen.innerHTML = '<p>press button for candy!!</p>';
+	winScreen.style.display = 'flex';
+}
+
+function showTryAgainScreen() {
+	const winScreen = document.getElementById('winner');
+	winScreen.innerHTML = '<p>tryyy again!!</p>';
+	winScreen.style.display = 'flex';
+
+	numberOfTrys.push('tried');
+	console.log(numberOfTrys.length);
+}
+
+function randomize(no) {
+	return Math.floor(Math.random() * no);
 }
 
 function windowResized() {
@@ -83,12 +134,11 @@ function serialEvent() {
 
 	if (dataValue === 26) {
 		initMachine();
+		return;
 	}
-	
 }
 
 function initMachine() {
-	console.log('init');
 	startSlotMachine(2);
 	return;
 }
