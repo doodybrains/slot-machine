0e7fa78d68eb397fcfd0dc462dc62c7ab4ef7375
Emma Rae <doodybrainz@gmail.com>
2018-10-23 15:23:54 -0400

    cleanup! (#1)

--- setup.ino
+++ setup.ino
@@ -2,7 +2,7 @@
 
 Servo myservo;
 int pos = 0;
-
+int p5Message;
 void setup() {
     Serial.begin(9600);
     pinMode(2, INPUT);
@@ -13,9 +13,11 @@ void loop() {
     int joystick = analogRead(A1);
     int mappedJoystickValue = map(joystick, 0, 1023, 0, 255);
     int buttonState = digitalRead(2);
-    
 
-    if (buttonState == 1) {
+
+    
+    if (buttonState == 1 && Serial.available() > 0) {
+     p5Message = Serial.read();
      for (pos = 0; pos <= 160; pos += 1) { 
         myservo.write(pos);
         delay(10);  
