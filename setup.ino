88655616340c2d128a797796f9de72987d95fe9e
doodybrains <doodybrainz@gmail.com>
2018-10-22 23:19:36 -0400

    Update code monday night

--- setup.ino
+++ setup.ino
@@ -1,13 +1,32 @@
+#include <Servo.h>
+
+Servo myservo;
+int pos = 0;
+
 void setup() {
     Serial.begin(9600);
+    pinMode(2, INPUT);
+    myservo.attach(8); 
 }
 
 void loop() {
     int joystick = analogRead(A1);
     int mappedJoystickValue = map(joystick, 0, 1023, 0, 255);
+    int buttonState = digitalRead(2);
     
 
-    
+    if (buttonState == 1) {
+     for (pos = 0; pos <= 160; pos += 1) { 
+        myservo.write(pos);
+        delay(10);  
+      }
+      delay(5000);
+      for (pos = 160; pos >= 0; pos -= 1) { 
+        myservo.write(pos);
+        delay(10);
+      }
+    }
+
     if (mappedJoystickValue < 90) {
         Serial.write(70);
     }
